e29026dc6da64cf733301f3167d357d5450e7022
Leandro Liduvino <leandro.augusto460@gmail.com>
2026-02-28 10:37:03 -0300

    chore: remove auth-server and gateway configurations and related files

--- configs/create-certs.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p certs
-openssl req -x509 -newkey rsa:4096 -nodes -keyout certs/redis.key -out certs/redis.crt -days 365 -subj '/CN=localhost'
-
-sudo chmod 644 certs/redis.key
-sudo chmod 644 certs/redis.crt
-
-sudo chown 999:999 certs/redis.key
-sudo chown 999:999 certs/redis.crt
-sudo chown 999:999 users.acl
--- configs/deploy/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Starting nginx..."
-nginx -g 'daemon off;'
