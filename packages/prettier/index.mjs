168b1b1ad8aebe6ac292081ff66c9dd711542826
Leandro Liduvino <leandro.augusto460@gmail.com>
2026-02-28 11:04:38 -0300

    refactor: rename package to lidcode and update related configurations
    feat(eslint): add base, next, and react configurations for ESLint
    feat(prettier): add Prettier configuration
    feat(tsconfig): create base and Next.js specific TypeScript configurations
    fix(ui): rename UI package to @lidcode/ui

---
+++ packages/prettier/index.mjs
@@ -0,0 +1,17 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: false
+}
+
+export default config
